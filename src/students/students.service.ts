2d21550c4bc49f7c25cd6c0addf22460df156703
spykelionel <social.lionel@gmail.com>
2024-01-17 12:16:35 +0100

    update student service

--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateStudentDto } from './dto/create-student.dto';
-import { UpdateStudentDto } from './dto/update-student.dto';
+import { Student } from './entities/student.entity';
+import { IStudent } from './student.interface';
 
 @Injectable()
-export class StudentsService {
-  create(createStudentDto: CreateStudentDto) {
-    return 'This action adds a new student';
+export class StudentsService implements IStudent {
+  constructor(
+    @InjectRepository(Student)
+    private readonly studentRepository: Repository<Student>,
+  ) {}
+  create(createStudentDto: CreateStudentDto): Promise<any> {
+    return this.studentRepository.insert(createStudentDto);
   }
-
-  findAll() {
-    return `This action returns all students`;
+  findAll(): Promise<Student[] | []> {
+    return this.studentRepository.find();
   }
-
-  findOne(id: number) {
-    return `This action returns a #${id} student`;
+  findOne(id: number): Promise<{} | Student> {
+    return this.studentRepository.findOne({ where: { id } });
   }
-
-  update(id: number, updateStudentDto: UpdateStudentDto) {
-    return `This action updates a #${id} student`;
+  update(id: number, updateStudentDto: { name?: string }): Promise<any> {
+    return this.studentRepository.update(id, updateStudentDto);
   }
-
-  remove(id: number) {
-    return `This action removes a #${id} student`;
+  remove(id: number): Promise<any> {
+    return this.studentRepository.delete(id);
   }
 }
