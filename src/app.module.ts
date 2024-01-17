fa1093845dff689c43776fa702d1a8be13b67625
spykelionel <social.lionel@gmail.com>
2024-01-17 04:57:36 +0100

    add resources - schools - students - departments - courses - grades

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SchoolsModule } from './schools/schools.module';
+import { StudentsModule } from './students/students.module';
+import { DepartmentsModule } from './departments/departments.module';
+import { CoursesModule } from './courses/courses.module';
+import { GradesModule } from './grades/grades.module';
 
 @Module({
-  imports: [],
+  imports: [SchoolsModule, StudentsModule, DepartmentsModule, CoursesModule, GradesModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/grades/grades.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { GradesService } from './grades.service';
+import { CreateGradeDto } from './dto/create-grade.dto';
+import { UpdateGradeDto } from './dto/update-grade.dto';
+
+@Controller('grades')
+export class GradesController {
+  constructor(private readonly gradesService: GradesService) {}
+
+  @Post()
+  create(@Body() createGradeDto: CreateGradeDto) {
+    return this.gradesService.create(createGradeDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.gradesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.gradesService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateGradeDto: UpdateGradeDto) {
+    return this.gradesService.update(+id, updateGradeDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.gradesService.remove(+id);
+  }
+}
