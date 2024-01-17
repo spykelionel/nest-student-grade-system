9864f160bd8a29eb6b3d2b4824375cf713633d82
spykelionel <social.lionel@gmail.com>
2024-01-17 08:10:19 +0100

    add datasource to root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from 'db/data-source';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoursesModule } from './courses/courses.module';
+import { DepartmentsModule } from './departments/departments.module';
+import { GradesModule } from './grades/grades.module';
 import { SchoolsModule } from './schools/schools.module';
 import { StudentsModule } from './students/students.module';
-import { DepartmentsModule } from './departments/departments.module';
-import { CoursesModule } from './courses/courses.module';
-import { GradesModule } from './grades/grades.module';
 
 @Module({
-  imports: [SchoolsModule, StudentsModule, DepartmentsModule, CoursesModule, GradesModule],
+  imports: [
+    TypeOrmModule.forRoot(dataSourceOptions),
+    SchoolsModule,
+    StudentsModule,
+    DepartmentsModule,
+    CoursesModule,
+    GradesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
